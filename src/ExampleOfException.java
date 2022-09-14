3b8e2586d03e29e527ae7096696cb3de48ac47eb
Ranajit_Khandual <ranajitkhandual@gmail.com>
2022-09-14 22:31:42 +0530

    add some exception handling methods

--- src/ExampleOfException.java
+++ src/ExampleOfException.java
@@ -1,8 +1,7 @@
-import java.net.SocketTimeoutException;
-
 public class ExampleOfException {
   public static void main(String[] args) {
-    TryCatch();
+    // TryCatch();
+    TryCatchFinally();
   }
   public static void TryCatch(){
     try{
@@ -13,4 +12,16 @@ public class ExampleOfException {
     }
     System.out.println("execute rest of code");
   }
+  public static void TryCatchFinally(){
+    try{
+      int data = 10 / 0 ;
+      System.out.println(data);
+    }catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("the code has a ArrayIndexOutOfBoundsException");
+    }finally{
+      System.out.println("importand code");
+    }
+    System.out.println("execute rest of the code");
+  }
+  
 }
